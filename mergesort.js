b36a846eb3df0b67a49d16c4820e2dd2a3df0840
jes708 <jonschwarz@nyu.edu>
2016-04-22 13:03:22 -0400

    solved

--- mergesort.js
+++ mergesort.js
@@ -1,5 +1,5 @@
 function split(wholeArray) {
-    arr = wholeArray.slice()
+    var arr = wholeArray.slice()
     if (arr.length === 1) {
       return [arr];
     }
@@ -9,24 +9,78 @@ function split(wholeArray) {
 }
 
 function mergeSort(array) {
-  var currentArray = array;
-  var arr1 = array[0];
-  var arr2 = array[1];
-  var result = [];
+  // If our array is one or zero in length, then it is sorted
+  if (array.length < 2) {
+    return array;
+  }
+  // Otherwise, it needs to be split and sorted
+  var splits = split(array);
+  var left = splits[0];
+  var right = splits[1];
+  // We will attempt to merge the two halves
+  return merge(mergeSort(left), mergeSort(right));
+}
 
-  while (true){
-    if(arr1[0] < arr2[0]){
-      result.push(arr1.splice(0, 1)[0]);
-      if (!arr1.length){
-        return result.concat(arr2);
-      }
-    } 
-    else if (arr1[0] >= arr2[0]){
-      result.push(arr2.splice(0, 1)[0]);
-      if (!arr2.length){
-        return result.concat(arr1);
+
+
+  function merge(arr1, arr2) {
+  var result = [];
+    while (true){
+      if(arr1[0] < arr2[0]){
+        result.push(arr1.splice(0, 1)[0]);
+        if (!arr1.length){
+          return result.concat(arr2);
+        }
+      } 
+      else if (arr1[0] >= arr2[0]){
+        result.push(arr2.splice(0, 1)[0]);
+        if (!arr2.length){
+          return result.concat(arr1);
+        }
       }
     }
   }
 
-}
+
+
+
+
+    // if (arr[0].length !== 1) {
+    //   mergeSort(arr[0])
+    //   mergeSort(arr[1])
+    // }
+
+// var jkl = [];
+// function recursion(array) {
+//   var splitArray = split(array);
+//   if (splitArray[0].length > 1) {
+//     jkl.push(recursion(array[0]))
+//   }
+//   if (splitArray[1].length > 1) {
+//     jkl.push(recursion(array[1]))
+//   }
+//   return jkl
+// }
+
+
+
+
+
+  // function splitAll(array){
+  //   var timesToSplit = 0;
+
+  //   var module = 0;
+
+  //   var currentLength = array.length;
+
+  //   while (currentLength / 2 > 1){
+  //     module = currentLength % 2;
+  //     currentLength /= 2;
+  //     timesToSplit++;
+  //   }
+
+  //   if(module !== 0){
+  //     timesToSplit++;
+  //   }
+  //   return timesToSplit;
+  // }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,6 +1,6 @@
 describe('Merge Sort', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
-    expect(mergeSort([[1, 3, 5, 8], [2, 4, 6, 7]])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(merger([1, 3, 5, 8], [2, 4, 6, 7])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 });
 
@@ -10,3 +10,40 @@ describe('Split Array function', function() {
   });
 });
 
+// 
+
+// length 8 = 3 steps
+// [1,2,3,4,5,6,7,8]
+// [[1,2,3,4],[5,6,7,8]]
+// [[[1,2],[3,4]],[[5,6],[7,8]]]
+// [[[[1],[2]]]]
+
+// length 7 = 3 steps
+// [1,2,3,4,5,6,7]
+// [1,2,3] [4, 5, 6, 7]
+// [1] [2,3] [4,5] [6,7]
+// 3
+
+// length 9 = Math.ceil(Math.log2(9)) = 4 steps
+// [1,2,3,4,5,6,7,8,9]
+// [[1,2,3,4],[5,6,7,8,9]]
+// [[[1,2],[3,4]],[[5,6],[7,8,9]]]
+
+
+// mergeSort([8,3,6,1]) = [1,3,6,8]
+// mergeSort([8,3,6,1]) = merge([3,8], [1,6])
+// mergeSort([8,3,6,1]) = merge(mergeSort([8, 3]), mergeSort[6, 1])
+
+// mergeSort([8,3]) = [3, 8]
+// mergeSort([8,3]) = merge([8], [3])
+// mergeSort([8,3]) = merge(mergeSort([8]), mergeSort[3])
+
+// mergeSort([8]) = [8]
+// mergeSort([3]) = [3]
+
+// mergeSort([6,1]) = [1, 6]
+// mergeSort([6,1]) = merge([6], [1])
+// mergeSort([6,1]) = merge(mergeSort([6]), mergeSort[1])
+
+// mergeSort([6]) = [6]
+// mergeSort([1]) = [1]
